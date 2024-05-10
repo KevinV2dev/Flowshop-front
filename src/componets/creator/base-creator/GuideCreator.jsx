0c4301641b998957d0180732d2da0e830fa6f1e1
spell852 <vegaskevin46@gmail.com>
2024-05-09 20:05:45 -0400

    editor

--- src/componets/creator/base-creator/GuideCreator.jsx
+++ src/componets/creator/base-creator/GuideCreator.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import '../creator-componets.css'
+
 const GuideCreator = () => {
   return (
     <>
@@ -12,6 +15,23 @@ const GuideCreator = () => {
 
       <div className='quill-container'>
         <div className='btn-quills'>
+        <CKEditor
+                    editor={ ClassicEditor }
+                    data="<p>Hello from CKEditor&nbsp;5!</p>"
+                    onReady={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event ) => {
+                        console.log( event );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
         </div>
 
         <div className='editor-quills'>
